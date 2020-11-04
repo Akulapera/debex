f69cd3facb51e744a0d4ecb9ec847c1617ea6988
Akulapera <porokhnia.anastasiya@gmail.com>
2020-11-04 20:54:27 +0200

    price

--- src/test/java/FirsTest.java
+++ src/test/java/FirsTest.java
@@ -1,24 +1,41 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ISelect;
+import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import java.util.Collections;
+import org.testng.Assert;
 
-public class FirsTest {
-    WebDriver dr;
-
-    @BeforeMethod
-    public void setUp(){
-    dr=new ChromeDriver();
-    }
+public class FirsTest extends TestBase  {
 
     @Test
-    public  void openChromeTest(){
-       dr.get("https://www.tel-ran.com/");
+    public  void openChromeTest() throws Exception{
+
+        openSite("https://www.wildberries.ru/");
+        maximizeWindow();
+        clickMenu(By.cssSelector("[type=button]"));
+        clickElectronica(By.cssSelector("[href=\"/catalog/elektronika\"]"));
+        clickLaptops(By.cssSelector("#banner_13d8cd94-ed6b-4b57-92e0-da2b7a282e94"));
+        clickLaptops(By.cssSelector("#price"));
+        //dr.findElement(By.cssSelector(".ru")).click();
+        Thread.sleep(6000);
+        List<WebElement> elements = dr.findElements(By.xpath("//div[@class='j-cataloger-price']/span[@class='price']/ins[@class='lower-price']"));
+        List<Integer> prices = new ArrayList<>();
+
+        for (WebElement element:elements) {
+
+            List<String> result = Splitter.on(" ").trimResults().splitToList(element.getText());
+            String price = Joiner.on("").join(result.get(0), result.get(1));
+            Integer price_int = Integer.parseInt(price);
+
+            prices.add(price_int);
+        }
+        List sorted = new ArrayList<>(prices);
+        Collections.sort(prices);
+        Assert.assertEquals(prices, sorted);
     }
 
-     @AfterMethod
-    public  void tearDown(){
-    dr.quit();
-     }
 }
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver dr;
+
+    @BeforeMethod
+    public void setUp(){
+    dr=new ChromeDriver();
+    dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+   public  void tearDown(){
+   dr.quit();
+    }
+
+    private void clickPrice(By locator) {
+        dr.findElement(locator).click();
+    }
+
+    protected void clickLaptops(By locator) {
+        clickPrice(locator);
+    }
+
+    protected void clickElectronica(By locator) {
+        clickLaptops(locator);
+    }
+
+    protected void clickMenu(By locator) {
+        clickLaptops(locator);
+    }
+
+    protected void maximizeWindow() {
+        dr.manage().window().maximize();
+    }
+
+    protected void openSite(String url) {
+        dr.get(url);
+    }
+}
