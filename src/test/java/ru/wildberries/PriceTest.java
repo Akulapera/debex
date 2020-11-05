c81e9a9e31dfdb83e7ceb8c561bf0bdd19e72484
Akulapera <porokhnia.anastasiya@gmail.com>
2020-11-05 19:11:12 +0200

    before select Browser

--- src/test/java/ru/wildberries/PriceTest.java
+++ src/test/java/ru/wildberries/PriceTest.java
@@ -26,23 +26,4 @@ public class PriceTest extends TestBase {
 
         Assert.assertEquals(price_parser.get_prices(), price_parser.get_prices_sorted());
     }
-
-    @Test
-    public  void sortingDiscountTest()    throws Exception{
-        openSite("https://www.wildberries.ru/");
-        maximizeWindow();
-        pause(3000);
-        clickMenu(By.cssSelector("[type=button]"));
-        clickElectronica(By.cssSelector("[href=\"/catalog/elektronika\"]"));
-        clickLaptops(By.cssSelector("#banner_13d8cd94-ed6b-4b57-92e0-da2b7a282e94"));
-        clickDiscount(By.cssSelector("#sale"));
-        pause(6000);
-
-        List<WebElement> elements = getWebElements(By.xpath("//div[@class='j-cataloger-price']/span[@class='price']/span[@class='price-old-block']/span[@class='price-sale active']"));
-        Parser discount_parser = new Parser(elements);
-        discount_parser.parse_discounts();
-        discount_parser.sort_discounts();
-
-        Assert.assertEquals(discount_parser.getDiscounts(), discount_parser.getDiscounts_sorted());
-    }
 }
--- src/test/java/ru/wildberries/TestBase.java
+++ src/test/java/ru/wildberries/TestBase.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
