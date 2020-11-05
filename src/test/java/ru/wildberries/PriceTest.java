42188b7567f6c4dbea14eea16bfb6464bf46a153
Akulapera <porokhnia.anastasiya@gmail.com>
2020-11-05 14:44:26 +0200

    with parser2

--- src/test/java/ru/wildberries/PriceTest.java
+++ src/test/java/ru/wildberries/PriceTest.java
@@ -1,14 +1,14 @@
+package ru.wildberries;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebElement;
-import java.util.ArrayList;
+
 import java.util.List;
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-import java.util.Collections;
+
 import org.testng.Assert;
 
-public class PriceTest extends TestBase  {
+public class PriceTest extends TestBase {
 
     @Test
     public  void openChromeTest() throws Exception{
@@ -20,18 +20,12 @@ public class PriceTest extends TestBase  {
         clickLaptops(By.cssSelector("#banner_13d8cd94-ed6b-4b57-92e0-da2b7a282e94"));
         clickLaptops(By.cssSelector("#price"));
         Thread.sleep(6000);
-        List<WebElement> elements = dr.findElements(By.xpath("//div[@class='j-cataloger-price']/span[@class='price']/ins[@class='lower-price']"));
-        List<Integer> prices = new ArrayList<>();
 
-        for (WebElement element:elements) {
-            List<String> result = Splitter.on(" ").trimResults().splitToList(element.getText());
-            String price = Joiner.on("").join(result.get(0), result.get(1));
-            Integer price_int = Integer.parseInt(price);
-            prices.add(price_int);
-        }
-        List sorted = new ArrayList<>(prices);
-        Collections.sort(prices);
-        Assert.assertEquals(prices, sorted);
+        List<WebElement> elements = dr.findElements(By.xpath("//div[@class='j-cataloger-price']/span[@class='price']/ins[@class='lower-price']"));
+        ParserPrice price_parser = new ParserPrice(elements);
+        price_parser.parse();
+        price_parser.sort();
+        Assert.assertEquals(price_parser.get_prices(), price_parser.get_prices_sorted());
     }
 //div[@class='j-cataloger-price']/span[@class='price']/span[@class='price-old-block']/span[@class='price-sale active']
 }
